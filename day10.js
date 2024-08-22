ac93b45cbe0dc07fa68a4745af6e075a0f1df039
Muhammad Zohaib <muhammad.zohaib@miletap.com>
2024-08-22 21:34:53 +0500

    WHILE LOOP - FOR LOOP

---
+++ day10.js
@@ -0,0 +1,36 @@
+//  ITRATIONS
+
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+
+// For Loop
+
+let studentName = "Ali";
+console.log(studentName);
+
+// var tableNum = prompt("Enter table number");
+
+var tableNum = 2;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${tableNum} X ${i} = ${tableNum * i}`);
+}
+
+
+
+
+//While Loop
+
+let k = 0;
+
+while (k <= 10) {
+    console.log(k);
+    k++;
+}
+
+
+// 2 X 1 = 2
+// 2 X 2 = 4
+// 2 X 3 = 6
+
